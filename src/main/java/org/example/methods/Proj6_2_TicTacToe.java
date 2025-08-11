25a3629da2a44591087584ece9d92e7df1d7ad6a
Paulo Bueno <paulob.bruno@gmail.com>
2025-08-11 00:38:54 -0300

    Refactoring exercise

--- src/main/java/org/example/methods/Proj6_2_TicTacToe.java
+++ src/main/java/org/example/methods/Proj6_2_TicTacToe.java
@@ -1,159 +1,130 @@
 package org.example.methods;
 
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Proj6_2_TicTacToe {
-
-    private static String BOARD_PLACE_HOLDER = "   ";
+    static int BOARD_SIZE = 3;
+    static char FIRST_PLAYER = 'X';
+    static char SECOND_PLAYER = 'O';
 
     public static void main(String[] args) {
-        String[][] board = createBoard();
-        int round = 0;
-        int[] move = new int[2];
-        String player;
+        String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
+        int turn = 1;
 
-        showBoard(board);
-        while (!isGameFinished(board)) {
-            round++;
-            player = getPlayer(round);
-            move = getMove();
-            insertMoveIntoBoard(board, move, player);
+        while (!gameOver(board)) {
             showBoard(board);
+            insertMove(board, turn);
+            turn++;
         }
 
-        System.out.println("GAME FINISHED!");
+        String winner = getWinner(board);
+        if (!winner.isBlank()) {
+            System.out.println("Congratulations! the winner is " + winner);
+        }  else {
+            System.out.println("Too bad. It is a DRAW");
+        }
 
     }
 
-    public static String[][] createBoard() {
-        String[][] board = new String[3][3];
+    public static boolean gameOver(String[][] board) {
+        return allCellsPopulated(board)
+                || samePlayerInAnyRow(board)
+                || samePlayerInAnyColumn(board)
+                || samePlayerInAnyDiagonal(board);
+    }
+
+    public static boolean allCellsPopulated(String[][] board) {
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = BOARD_PLACE_HOLDER;
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j].isBlank()) {
+                    return false;
+                }
             }
         }
-        return board;
+        return true;
     }
 
-    public static String getPlayer(int roundNumber) {
-        if (roundNumber % 2 == 0) {
-            return "X";
-        } else {
-            return "O";
+    public static boolean samePlayerInAnyRow(String[][] board) {
+        boolean sameInRow;
+        for (int i = 0; i < board.length; i++) {
+            sameInRow = true;
+
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j].isBlank()) {
+                    sameInRow = false;
+                    break;
+                }
+                if (j > 0 && !board[i][j-1].equals(board[i][j])) {
+                    sameInRow = false;
+                    break;
+                }
+            }
+
+            if (sameInRow) {
+                return true;
+            }
+
         }
+
+        return false;
+
     }
 
-    public static void insertMoveIntoBoard(String[][] board,
-                                                 int[] move,
-                                                 String player) {
+    public static boolean samePlayerInAnyColumn(String[][] board) {
+        boolean sameInColumn;
 
-        if (isValidMove(board, move)) {
-            board[move[0]][move[1]] = " " + player + " ";
-        }
-    }
+        for (int j = 0; j < board[0].length; j++) {
+            sameInColumn = true;
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][j].isBlank()) {
+                    sameInColumn = false;
+                    break;
+                }
+                if (i > 0 && !board[i-1][j].equals(board[i][j])) {
+                    sameInColumn = false;
+                    break;
+                }
+            }
 
-    public static boolean isValidMove(String[][] board,
-                                      int[] move) {
-        if ( move[0] > board.length - 1 && move[1] > board[0].length -1 ) {
-            System.out.println("Move not valid. Try again.");
-            return false;
+            if (sameInColumn) {
+                return true;
+            }
+
         }
-        if (Objects.equals(board[move[0]][move[1]], BOARD_PLACE_HOLDER)) {
-            return true;
-        }
-        System.out.println("Move not valid. Try again.");
+
         return false;
     }
 
-    public static int[] getMove() {
-        int[] movement = new int[2];
-        String[] move;
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("Insert the row and collum as x,y format: ");
-        move = keyboard.nextLine().split(",");
-
-        for (int i = 0; i < movement.length; i++) {
-            movement[i] = Integer.parseInt(move[i].trim());
-        }
-
-        return movement;
-    }
-
-
-    public static void showBoard(String[][] board) {
+    public static boolean samePlayerInAnyDiagonal(String[][] board) {
+        boolean sameInDiagonal = false;
 
         for (int i = 0; i < board.length; i++) {
-            if (i > 0) {
-                System.out.println("-".repeat(15));
+            sameInDiagonal = true;
+            if (board[i][i].isBlank()) {
+                sameInDiagonal = false;
+                break;
             }
-            System.out.println(String.join(" | ", board[i]));
-        }
-
-    }
-
-    public static boolean isGameFinished(String[][] board) {
-        return checkDiagonalFinish(board) ||
-                checkHorizontalFinish(board) ||
-                checkVerticalFinish(board);
-    }
-
-    public static boolean checkHorizontalFinish(String[][] board) {
-        boolean finished = true;
-        for (String[] row : board) {
-            for (int j = 1; j < row.length; j++) {
-                if (Objects.equals(row[j - 1], BOARD_PLACE_HOLDER) ||
-                        Objects.equals(row[j], BOARD_PLACE_HOLDER) ||
-                        !Objects.equals(row[j - 1], row[j])) {
-                    finished = false;
-                    break;
-                }
-            }
-            if (finished) {
-                return finished;
-            }
-        }
-        return finished;
-    }
-
-    public static boolean checkVerticalFinish(String[][] board) {
-        boolean finished = true;
-        for (int j = 0; j < board[0].length; j++) {
-            for (int i = 1; i < board.length; i++) {
-                if (Objects.equals(board[i - 1][j], BOARD_PLACE_HOLDER) ||
-                        Objects.equals(board[i][j], BOARD_PLACE_HOLDER) ||
-                        !Objects.equals(board[i - 1][j], board[i][j])) {
-                    finished = false;
-                    break;
-                }
-            }
-            if (finished) {
-                return true;
-            }
-        }
-        return finished;
-    }
-
-    public static boolean checkDiagonalFinish(String[][] board) {
-        boolean finished = true;
-        for (int i = 1; i < board.length; i++) {
-            for (int j = 1; j < board[i].length; j++) {
-                if ( i == j) {
-                    if (Objects.equals(board[i - 1][j - 1], BOARD_PLACE_HOLDER) ||
-                            Objects.equals(board[i][j], BOARD_PLACE_HOLDER) ||
-                            !Objects.equals(board[i - 1][j - 1], board[i][j])
-                    ) {
-                        finished = false;
-                        break;
-                    }
-                }
-            }
-            if (!finished) {
+            if (i > 0 && !board[i-1][i-1].equals(board[i][i])) {
+                sameInDiagonal = false;
                 break;
             }
         }
-        return finished;
+
+        if (sameInDiagonal) {
+            return true;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            sameInDiagonal = true;
+            if (board[i][i].isBlank()) {
+                sameInDiagonal = false;
+                break;
+            }
+            if (i > 0 && !board[BOARD_SIZE-i][i-1].equals(board[BOARD_SIZE-1-i][i])) {
+                sameInDiagonal = false;
+                break;
+            }
+        }
+
+        return sameInDiagonal;
     }
 
 }
